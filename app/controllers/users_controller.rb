6e1847d9e077f7e7d1cedf42c4b6616e0a5211ff
keisuketominaga <keisuke.tominaga@icloud.com>
2015-12-21 15:31:17 +0000

    1st user-profile submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user?, only: [:edit, :update, :destroy]
+  
   def new
     @user = User.new
   end
@@ -9,6 +12,7 @@ class UsersController < ApplicationController
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
+      
     else
       render 'new'
     end
@@ -17,6 +21,32 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if current_user == @user
+      @user.update(user_params)
+      flash[:success] = "Your profile has been updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+    
+  def destroy
+    @user = User.find(params[:id])
+    if current_user == @user
+        @user.destroy
+        flash[:success] = "Your account has been deleted"
+        redirect_to root_url
+    end
+  end
+    
+  
   
   private
   
